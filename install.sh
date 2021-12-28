0e616d1e2f3ee0445de083d723f8521d7771ae46
izhostMX <91051985+izhostMX@users.noreply.github.com>
2021-12-28 05:49:15 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/dash
+apt-get update -y
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
